aae317cb05ccb6749585be28ed721c36b9e6c297
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-11-21 18:52:53 -0300

    requisito 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+
+class Header extends Component {
+  render() {
+    const { email, expenses, exchange } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">Email: { email }</p>
+        <p data-testid="total-field">Despesa total: { expenses } <span data-testid="header-currency-field">{ exchange }</span></p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  exchange: PropTypes.string.isRequired,
+  expenses: PropTypes.number.isRequired,
+};
+
+export default Header;
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Header from '../components/Header'
 
 class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return(
+      <div>
+        <Header 
+          email={ this.props.email }
+          expenses={ 0 }
+          exchange="BRL"
+        />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = state => ({
+  email: state.user.email
+});
+
+export default connect(mapStateToProps, null)(Wallet);
